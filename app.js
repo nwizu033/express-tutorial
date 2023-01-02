8a67baeccd89610805e8755185f1d2adf498f4f1
chinwizu <chinwianthony@gmail.com>
2023-01-02 08:15:59 +0100

    creating server and sending response

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// to listen to a request, we use
+app.listen(3000);
+
+// to get a request 
+app.get('/', (req,res) => {
+    res.sendFile('./pages/index.html', {root: __dirname});
+});
+
+app.get('/contact', (req,res) => {
+    res.sendFile('./pages/contact.html', {root: __dirname});
+});
+
+
+
+
